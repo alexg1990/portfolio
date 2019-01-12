0a4212a48be88a9d40931b114f635470329c97f3
alexg1990 <alexg1990@gmx.net>
2019-01-12 09:11:07 +0100

    animation added about

--- script.js
+++ script.js
@@ -20,20 +20,22 @@ window.onload = function () {
       navbar.classList.remove('navSticky');
       about.style.marginTop = '0px';
     }
+    if(window.pageYOffset >= aboutOffset - 20) {
+      animLeftRight('titleAbout');
+    }
     switch (true) {
       case (window.pageYOffset >= aboutOffset && window.pageYOffset <= portfolioOffset):
         navHome.style.color = 'white';
         navAbout.style.color = '#17BF94';
         navPortfolio.style.color = 'white';
         navContact.style.color = 'white';
-        document.getElementById('titleAbout').classList.add('solidAnim');
+
         break;
       case (window.pageYOffset >= portfolioOffset && window.pageYOffset <= contactOffset):
         navHome.style.color = 'white';
         navAbout.style.color = 'white';
         navPortfolio.style.color = '#17BF94';
         navContact.style.color = 'white';
-
         break;
       case (window.pageYOffset >= contactOffset):
         navHome.style.color = 'white';
@@ -48,4 +50,8 @@ window.onload = function () {
         navContact.style.color = 'white';
     }
   };
+  function animLeftRight (obj) {
+    let objId = document.getElementById(obj);
+    objId.classList.add('solidAnim');
+  }
 };
